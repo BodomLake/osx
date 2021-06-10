ff653104900d82685e00deaa5a5266411add05bd
BodomLake <7922944021@qq.com>
2021-06-10 11:44:54 +0800

    '修改初始化数据的引入方式'

---
+++ src/components/ndg/app.js
@@ -0,0 +1,429 @@
+export var desks = [
+         {
+           order: 1,
+           boxes: [
+             {
+               id: "5a4d297c-d628-4d37-9571-a865d1318b82",
+               name: "第1个盒子",
+               apps: [
+                 {
+                   name: "edge",
+                   id: "5b380a47-651d-4bc6-92b3-d993f92fb57f"
+                 },
+                 {
+                   name: "chrome",
+                   id: "a43909d4-ac54-4dab-8fa8-c26915f03efc"
+                 },
+                 {
+                   name: "Safari",
+                   id: "6e272b33-e19b-4f17-a080-50f93267e812"
+                 },
+                 {
+                   name: "jiangxi",
+                   id: "7e1aa906-fa1c-4de6-a98d-5ab5d2044b63"
+                 },
+                 {
+                   name: "QQyouxiang",
+                   id: "b5e74770-d6d8-4470-9b9e-6935bb4961bd"
+                 },
+                 {
+                   name: "xinlang",
+                   id: "3338f78b-7356-4341-b50f-229358c516d1"
+                 },
+                 {
+                   name: "taobao",
+                   id: "54de2d1d-cae6-4e04-94fe-387f420dfe24"
+                 },
+                 {
+                   name: "app-store",
+                   id: "df71f19d-44a2-408e-98e4-3366080119b0"
+                 },
+                 {
+                   name: "xunlei_nomarl",
+                   id: "cbe91ea8-c837-4f6f-b238-274ad3daee79"
+                 },
+                 {
+                   name: "Bosszhipin",
+                   id: "0435e9e1-5654-413f-8a78-b540568c68e0"
+                 },
+                 {
+                   name: "wangyiyun",
+                   id: "518a970f-a45d-45bb-8a5f-2db3b8498206"
+                 },
+                 {
+                   name: "sougoup",
+                   id: "3b82e493-5e65-4de8-aa37-01f68803231d"
+                 },
+                 {
+                   name: "wangyiyun",
+                   id: "44b49ad6-3b19-4c45-8970-aa95127262d8"
+                 },
+                 {
+                   name: "douyin",
+                   id: "9b3b7c3d-5775-4575-bcbd-c37ddd35cb93"
+                 },
+                 {
+                   name: "shezhi",
+                   id: "ab5b7dd6-330a-47ea-87da-ad4e6a4e8a51"
+                 },
+                 {
+                   name: "tianmao",
+                   id: "f97bcca4-a15c-44dd-a0e6-d430e4710060"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "8c28ff13-1afc-4a80-9db6-fa7c1f9888ba",
+               name: "第2个盒子",
+               apps: [
+                 {
+                   name: "QQ",
+                   id: "e6c53e8a-2e21-410e-9e9a-b5f9775c2a14"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "834684d7-db3d-40f0-9838-a8b78e78fa1d",
+               name: "第3个盒子",
+               apps: [
+                 {
+                   name: "we-chat",
+                   id: "d63bc5f8-2e6d-4b80-965a-e1fc401c9b38"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "f6184918-8832-4603-9947-d0520e73ab0b",
+               name: "第4个盒子",
+               apps: [
+                 {
+                   name: "pdd",
+                   id: "6a16fd19-bc5e-41cb-ae82-8d43a5304951"
+                 },
+                 {
+                   name: "51job",
+                   id: "7840f0ad-8236-4eff-95f5-f6bb8eff301b"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "b7daa12e-83a1-4230-b6c1-9bdacafc4c42",
+               name: "第5个盒子",
+               apps: [
+                 {
+                   name: "amap",
+                   id: "9d24556f-1edc-48a7-810e-9d0d96a24469"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "7e96a648-423a-4b80-9cce-4166b55a370f",
+               name: "第6个盒子",
+               apps: [
+                 {
+                   name: "zhihu",
+                   id: "a892b7e8-efce-420a-a8bf-d23871822c42"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "3b6d51ea-1127-4374-bdc1-7a475658ae76",
+               name: "第7个盒子",
+               apps: [
+                 {
+                   name: "dingding",
+                   id: "eb350df2-85b5-4a9f-9b6a-01049b1833df"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "1617791e-1ab6-4568-a924-33f3a686cbd4",
+               name: "第9个盒子",
+               apps: [
+                 {
+                   name: "sougoup",
+                   id: "51680290-780b-412d-801a-3b5893ef5a1c"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "971c84fd-0aae-4d40-816f-170841498ee2",
+               name: "第10个盒子",
+               apps: [
+                 {
+                   name: "eliaomo",
+                   id: "621bba22-a93c-4a15-9089-f27ae0101db7"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "42883f8f-1f10-4162-aae7-c7adb2fcd710",
+               name: "第11个盒子",
+               apps: [
+                 {
+                   name: "bilibili",
+                   id: "479f1296-dad8-4d5f-842f-adb4c11cc39e"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "d1990732-d1df-4666-adaa-7636b41be54f",
+               name: "第12个盒子",
+               apps: [
+                 {
+                   name: "souhu",
+                   id: "4f2aa476-e4cd-4137-838a-030f592049d2"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "bae1ea10-6770-4bfa-812a-7921c12e8e19",
+               name: "第13个盒子",
+               apps: [
+                 {
+                   name: "zhongguodianxin",
+                   id: "34c3d7e6-1428-400e-8267-912cde06a18c"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "929d4fe2-ce56-44b1-b1f1-d755ab69e750",
+               name: "第14个盒子",
+               apps: [
+                 {
+                   name: "zhongguoliantong",
+                   id: "a6838d14-aa54-4f12-9a1a-000d7a9f1e40"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             }
+           ],
+           name: "第1个桌面",
+           id: "1f03352d-f43f-42c0-b66b-255af2e545b9"
+         },
+         // 第二个桌面
+         {
+           order: 2,
+           boxes: [
+             {
+               id: "77599f87-63da-4a40-8180-67f887eb951e",
+               name: "第1个盒子",
+               apps: [
+                 {
+                   name: "jingdong",
+                   id: "862960ea-35da-4d43-9261-ff817dc30656"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "d959625c-7d3a-4265-9710-e9a80a7a660f",
+               name: "第2个盒子",
+               apps: [
+                 {
+                   name: "xueqiu",
+                   id: "40cb75ce-1490-485b-8300-c397fa630dfc"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "520745bf-61aa-4be4-b66f-7b6f9159cea0",
+               name: "第3个盒子",
+               apps: [
+                 {
+                   name: "git1",
+                   id: "81216033-be9e-4808-bf80-ecdbb489bb88"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "e88b8ffd-b5e5-4326-a912-3f169004e808",
+               name: "第4个盒子",
+               apps: [
+                 {
+                   name: "gongshangyinhang",
+                   id: "fc7f5b1e-97ae-4bfd-ad0a-c05e835aedb6"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "385c82d7-42e1-4535-95cc-5adb181c3952",
+               name: "第5个盒子",
+               apps: [
+                 {
+                   name: "jiansheyinhang",
+                   id: "2fbed798-5b3e-4b76-a61e-385acb9ddc33"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "94100926-be47-45ae-a575-d2d01854e4bf",
+               name: "第6个盒子",
+               apps: [
+                 {
+                   name: "zhongguoyinhang",
+                   id: "794f51d7-553c-4945-b176-d9e1dc1a2395"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "af7fd5a1-fa8e-4439-9fce-a3255330263b",
+               name: "第7个盒子",
+               apps: [
+                 {
+                   name: "beijingyinhang",
+                   id: "d11bf07d-e494-4eb8-bb09-246b62a76709"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "a9195052-f5d0-4080-9f3f-69313eb4c781",
+               name: "第8个盒子",
+               apps: [
+                 {
+                   name: "zhifubao",
+                   id: "d9240137-c36d-42fe-8905-a4c5e19387df"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "cf8de61b-e900-41d6-a69a-aa5bde01b22a",
+               name: "第9个盒子",
+               apps: [
+                 {
+                   name: "jisuanqi",
+                   id: "f4f1a266-043f-4c48-b4b9-a55704fb51bc"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "bbafeca7-ae2f-4823-ad20-23aa6396fd4e",
+               name: "第10个盒子",
+               apps: [
+                 {
+                   name: "dongfangcaifuwang",
+                   id: "d126d93d-cc77-4f5c-8fc3-a96d8d68da55"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "98820344-0c8c-466a-9faa-c310556f5f42",
+               name: "第11个盒子",
+               apps: [
+                 {
+                   name: "tonghuashun",
+                   id: "092b4fcc-1a77-4f43-8729-24be5f1042e0"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "58b3215c-fc74-4f25-92a6-f6ff6bd69992",
+               name: "第12个盒子",
+               apps: [
+                 {
+                   name: "guotaijunan",
+                   id: "d3d126b0-96eb-4396-b3e2-11abd6b9fb1b"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "f05bde14-1cda-4f11-832e-d415fa6e4ebe",
+               name: "第13个盒子",
+               apps: [
+                 {
+                   name: "nongyeyinhang",
+                   id: "4fac11a6-570e-4875-b480-36b12ddecc9b"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             },
+             {
+               id: "cf012f21-4861-4a9d-81cc-f08915b04912",
+               name: "第14个盒子",
+               apps: [
+                 {
+                   name: "ziyuan102",
+                   id: "331b9e90-a776-4779-87f3-a290568b3c3c"
+                 }
+               ],
+               displayNo: 0,
+               groupAppLimit: 9
+             }
+           ],
+           name: "第2个桌面",
+           id: "7e6473d2-fcda-4e27-bb52-72ebdccdaa6a"
+         }
+       ];
+class Desk {
+  constructor(id, name, order) {
+    this.order = order;
+    this.boxes = [];
+    this.name = name;
+    this.id = id;
+  }
+}
+class Box {
+  constructor(id, name, groupAppLimit) {
+    this.id = id;
+    this.name = name;
+    this.apps = [];
+    // 小窗口/模态窗口 显示的组号 默认第一组
+    this.displayNo = 0;
+    this.groupAppLimit = GROUPAPPLIMIT;
+  }
+}
+class App {
+  constructor(id, name) {
+    this.name = name;
+    this.id = id;
+  }
+}
--- src/components/ndg/mixins/initMixin.js
+++ src/components/ndg/mixins/initMixin.js
@@ -1,122 +1,32 @@
-let desks = [];
-let deskNum = 2;
-let folderNum = 14;
-let appNum = 16;
-// top:0,bottom:0,left:0,right:0,x:0,y:0,width:0,height:0
+import { desks } from "../app.js";
 
-import {
-  BACKGROUND,
-  CONTAINER,
-  GROUPAPPLIMIT,
-} from "../common.js";
-class Desk {
-  constructor(id, name, order) {
-    this.order = order;
-    this.boxes = [];
-    this.name = name;
-    this.id = id;
-  }
-}
-class Box {
-  constructor(id, name, groupAppLimit) {
-    this.id = id;
-    this.name = name;
-    this.apps = [];
-    // 小窗口/模态窗口 显示的组号 默认第一组
-    this.displayNo = 0;
-    this.groupAppLimit = GROUPAPPLIMIT;
-  }
-}
-class App {
-  constructor(id, name) {
-    this.name = name;
-    this.id = id;
-  }
-}
-import {
-  stringify,
-  v4 as uuidv4
-} from "uuid";
+import { BACKGROUND, CONTAINER, GROUPAPPLIMIT } from "../common.js";
+
+import { stringify, v4 as uuidv4 } from "uuid";
+
 export default {
   // 初始化 二维数组
   beforeCreate() {
-    for (let i = 0; i < deskNum; i++) {
-      // 新建一个桌面
-      let desk = new Desk(uuidv4(), "第" + (i + 1) + "个桌面", i + 1);
-      // 遍历 该个桌面下的 多个盒子
-      for (let j = 0; j < folderNum; j++) {
-        // 新建一个盒子
-        let box = new Box(uuidv4(), "第" + (j + 1) + "个盒子");
-        // 再往该盒子内填充App
-        for (let k = 0; k < appNum; k++) {
-          // 新建一个app，作为填充物
-          // if (j == 1 || j == 3 || j == 4) continue;
-          let app = new App(uuidv4(), "");
-          box.apps.push(app);
-          if (k == 0 && j == 1) break;
-          if (k == 0 && j == 2) break;
-          if (k == 0 && j == 4) break;
-        }
-        //
-        desk.boxes.push(box);
-      }
-      // 填入桌面数组
-      desks.push(desk);
-      // desk[i].boxes.push(new Box(uuidv4(), '', {}));
-    }
-
-    desks[0].boxes[0].apps[0].name = "edge";
-    desks[0].boxes[0].apps[1].name = "xunlei_nomarl";
-    desks[0].boxes[0].apps[2].name = "Safari";
-    desks[0].boxes[0].apps[3].name = "jiangxi";
-    desks[0].boxes[0].apps[4].name = "zhongguoliantong";
-    desks[0].boxes[0].apps[5].name = "xinlang";
-    desks[0].boxes[0].apps[6].name = "taobao";
-    desks[0].boxes[0].apps[7].name = "app-store";
-    desks[0].boxes[0].apps[8].name = "we-chat";
-    desks[0].boxes[0].apps[9].name = "meituan";
-    desks[0].boxes[0].apps[10].name = "wangyiyun";
-
-    desks[0].boxes[0].apps[11].name = "sougoup"
-    desks[0].boxes[0].apps[12].name = "wangyiyun"
-    desks[0].boxes[0].apps[13].name = "douyin"
-    desks[0].boxes[0].apps[14].name = "shezhi"
-    desks[0].boxes[0].apps[15].name = "tianmao"
-
-    //
-    desks[0].boxes[1].apps[0].name = "QQ";
-    desks[0].boxes[2].apps[0].name = "chrome";
-    desks[0].boxes[4].apps[0].name = "amap";
-
-    desks[0].boxes[3].apps[0].name = "pdd";
-    desks[0].boxes[3].apps[1].name = "51job";
-
-    desks[1].boxes[0].apps[0].name = "jingdong";
-    desks[1].boxes[0].apps[1].name = "xueqiu";
-    desks[1].boxes[0].apps[2].name = "git1";
   },
   data() {
     return {
       desks,
-      // deskWidth: (deskNum * 100) + '%',
-      folderNum: folderNum,
-      appNum: appNum,
       currentDeskNum: 0,
       groupAppLimit: GROUPAPPLIMIT
     };
   },
   model: {
-    value: 'desks',
-    event: 'changeDesk'
+    value: "desks",
+    event: "changeDesk"
   },
   computed: {
     // 计算桌面总宽度
     deskWidth() {
-      return deskNum * 100 + "%";
+      return desks.length * 100 + "%";
     },
     deskShiftOffset() {
-      return 100 / deskNum;
-    },
+      return 100 / desks.length;
+    }
   },
   watch: {},
   methods: {
@@ -141,7 +51,8 @@ export default {
       let transform = () => {
         document.querySelector(
           "div." + BACKGROUND
-        ).style.transform = `translateX(-${this.deskShiftOffset * this.currentDeskNum}%)`;
+        ).style.transform = `translateX(-${this.deskShiftOffset *
+          this.currentDeskNum}%)`;
       };
       switch (orientation) {
         case "right":
